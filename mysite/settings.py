be09c5dad5ba23c12d56112e2eaf6e4e49733ba6
dinushka <dinushkapiyumal678@gmail.com>
2020-05-13 09:44:10 +0530

    Initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,15 +90,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#             'default': {
+#                 'ENGINE': 'django.db.backends.mysql',
+#                 'NAME': 'myblog',
+#                 'USER': 'root',
+#                 'PASSWORD': '',
+#                 'HOST':'localhost',
+#                 'PORT':'3306',
+#             }
+# }
+
 DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.mysql',
-                'NAME': 'myblog',
-                'USER': 'root',
-                'PASSWORD': '',
-                'HOST':'localhost',
-                'PORT':'3306',
-            }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
