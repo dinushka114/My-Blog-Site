a67828ab5e3741b48f54f6c35887283c58d2d414
dinushka <dinushkapiyumal678@gmail.com>
2020-05-18 10:11:31 +0530

    'allowed_host_listed'

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$-7@bslx6kdv^rhjs8z%=%5&gxi%5)^wcr5)nt2$14(z$vyva('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mytechdiary.herokuapp.com']
+ALLOWED_HOSTS = ['mytechdiary.herokuapp.com' , 'localhost']
 # ALLOWED_HOSTS = ['localhost']
 
 INTERNAL_IPS = [
